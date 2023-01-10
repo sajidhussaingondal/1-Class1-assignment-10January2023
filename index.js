fb59d60263b907457301c4972fc56249cb8645f0
sajidhussaingondal <sajidgondal@gmail.com>
2023-01-10 11:58:59 +0500

    Class 1 home work

---
+++ index.js
@@ -0,0 +1,19 @@
+var myNumber = 10; // Using var keyword, a variable has been initialized and assigned a value.
+console.log(myNumber); // Using console.log command, value has been printed in the console window of browser. Printed value is 10.
+
+myNumber = 9; // New value has been assigned to previous declared variable.
+console.log(myNumber); // Using console.log command, value has been printed in the console window of browser. No error was found, printed value is 9.
+
+let myNumber1 = 8; // Using let keyword, a variable has been initialized and assigned a value.
+console.log(myNumber1); // Using console.log command, value has been printed in the console window of browser. Printed value is 8.
+
+myNumber1 = 7; // New value has been assigned to previous initialized variable.
+console.log(myNumber1); // Using console.log command, value has been printed in the console window of browser. No error was found, printed value is 7.
+
+
+const myNumber2 = 6; // Using const keyword, a variable has been initialized and assigned a value.
+console.log(myNumber2); // Using console.log command, value has been printed in the console window of browser. Printed value is 6.
+/*
+myNumber2 = 5; // New value has been assigned to previous initialized variable.
+console.log(myNumber2); // Using console.log command, value has been printed in the console window of browser. Error was found. If a variable has been initialized
+// with const keyword, you can't reassign a value to it (assigned value will be one time only), it will give error.*/
